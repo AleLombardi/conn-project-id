8a7a95e51180bbc332cf99cc17bf5008fe45e7dd
AleLombardi <alessandro3lombardi@gmail.com>
2020-07-16 19:22:33 +0200

    update

--- src/main/java/connessione/test.java
+++ src/main/java/connessione/test.java
@@ -21,32 +21,25 @@ public class test {
    
     public static void main(String[] args) {
     	
-    	//Persona  prova = new Persona("al","lom"); 
+    	 
         
     	
     	
     	
     	Client client = ClientBuilder.newBuilder().build();
-        WebTarget target = client.target("http://localhost:8080/pw-lombardi/resources");
+        WebTarget target = client.target("http://localhost:8080/pw-lombardi/resources/api");
         Response response = target.request().get();
         String users = response.readEntity(String.class);
         //System.out.println(users);
        
         
         
-        
-        
-        
-        
-        
-        String resp = target.path("users")
-        		.resolveTemplate(users, args)
-                .request().accept(MediaType.APPLICATION_JSON)
-                
-                .get(String.class);
-                
-        System.out.println(resp);
-        //System.out.println(addr.getAddresses("id"));
+        String resp = target.path("users").resolveTemplate(users, args).request().accept(MediaType.APPLICATION_JSON)
+
+				.get(String.class);
+
+		System.out.println(resp);
+		// System.out.println(addr.getAddresses("id"));
        
     }
 
